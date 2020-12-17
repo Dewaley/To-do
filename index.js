a30b6a43fbf174790f49b650b0881a719a9b2ddb
mrwaccaff <36827870+mrwaccaff@users.noreply.github.com>
2020-12-17 20:24:32 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,19 @@ const form = document.querySelector('.input-form')
 let tasks = document.querySelector('.tasks')
 let dark = document.querySelector('.fa-moon')
 let light = document.querySelector('.fa-sun')
-let todo = document.querySelector('.todo-lists')
+let todo = document.querySelector('.todo-lists');
+let storageName = 'taskData';
+let taskTemplate = (data) => `
+<div class="new-list" draggable="true" id="${data.arrIndex}">
+    <input type="checkbox" name="" id=${data.id+'_'+data.arrIndex} ${data.checked ?? ''} class="checkbox">
+    <label for=${data.id+'_'+data.arrIndex}>
+        <span class="custom-checkbox"></span>
+        ${data.task}
+    </label>
+    <span class="delete-list"><i class="fas fa-times" aria-hidden="true"></i></span>
+</div>
+<slot class="add-new"></slot>
+`;
 
 dark.addEventListener('click', e => {
     light.style.opacity = "1"
@@ -39,27 +51,32 @@ add.addEventListener('click', e => {
         alert('Please enter a task')
     } else {
         let id = Date.now().toString()
-        let newTask = `
-<div class="new-list" draggable="true">
-    <input type="checkbox" name="" id=${id} class="checkbox">
-    <label for=${id}>
-        <span class="custom-checkbox"></span>
-        ${task}
-    </label>
-    <span class="delete-list"><i class="fas fa-times" aria-hidden="true"></i></span>
-</div>
-<slot class="add-new"></slot>
-`;
-        let addNewTask = document.querySelectorAll('.add-new');
-        let addNewField = addNewTask[addNewTask.length - 1];
-        addNewField.innerHTML = newTask;
-        input.value = "";
+        let renderData = {
+            id: id,
+            task: task,
+            checked: '',
+            arrIndex: countAll()
+        }
+        let newTask = taskTemplate(renderData);
+        let newData = { value: input.value, checkState: '' };
+        saveTask(newData);
+        render(newTask);
     }
 })
+
+function render(newTask) {
+    let addNewTask = document.querySelectorAll('.add-new');
+    let addNewField = addNewTask[addNewTask.length - 1];
+    addNewField.innerHTML = newTask;
+    input.value = "";
+}
+
 window.addEventListener('click', (e) => {
     let target = e.target;
     if (target.classList.contains('fa-times')) {
         target.closest('div').remove();
+        let arrIndex = parseInt(target.closest('div').id);
+        deleteTask(arrIndex);
         count()
     }
     if (target.classList.contains('fa-plus')) {
@@ -76,14 +93,18 @@ window.addEventListener('click', (e) => {
         }
     }
     if (target.classList.contains('checkbox')) {
-        count()
+        count();
+        let arrIndex = parseInt(target.closest('div').id);
+        updateTask(arrIndex);
     }
     if (target.classList.contains('clear-all')) {
         let slot = `<slot class="add-new"></slot>`
         tasks.innerHTML = slot
-        count()
+        count();
+        addToStorage([]);
     }
 })
+
 function count() {
     let countChecked = document.querySelectorAll('.checkbox:checked');
     let items = document.querySelectorAll('.new-list')
@@ -97,3 +118,81 @@ function count() {
         number.textContent = num + ' items ' + 'left'
     }
 }
+
+function countAll() {
+    return document.querySelectorAll('.checkbox').length;
+}
+
+function loadTask() {
+    let getData = window.localStorage.getItem(storageName);
+    if (getData === null) {
+        return baseData();
+    }
+
+    let decodeData = window.localStorage.getItem(storageName);
+    try {
+        decodeData = JSON.parse(decodeData);
+    } catch (e) {
+        return baseData();
+    }
+    if (decodeData.length === 0) { return 'empty_list'; }
+    decodeData.forEach((item, index) => {
+        let id = Date.now().toString();
+        let renderData = {
+            id: id,
+            task: item.value,
+            arrIndex: index,
+            checked: item.checkState
+        }
+        let newTask = taskTemplate(renderData);
+        render(newTask);
+
+    });
+
+    /*
+        let tempData = [
+            { data: 'School Feee', checkState: 'checked' }
+        ];
+        */
+}
+
+function baseData() {
+    let baseData = [];
+    window.localStorage.setItem(storageName, JSON.stringify(baseData));
+    return 'new_data_set';
+}
+
+function saveTask(newData) {
+    let decodeData = getStorage();
+    decodeData.push(newData);
+    addToStorage(decodeData);
+}
+
+function getStorage() {
+    let getData = localStorage.getItem(storageName);
+    return JSON.parse(getData);
+}
+addToStorage = arr => localStorage.setItem(storageName, JSON.stringify(arr));
+
+function deleteTask(arrIndex) {
+    let data = getStorage();
+    data.splice(arrIndex, 1);
+    addToStorage(data);
+    updateTaskDivId();
+
+}
+
+function updateTask(arrIndex) {
+    let data = getStorage();
+    let checked = data[arrIndex].checkState == 'checked' ? '' : 'checked';
+    console.log(checked)
+    data[arrIndex].checkState = checked;
+    addToStorage(data)
+
+}
+
+function updateTaskDivId() {
+    document.querySelectorAll('.new-list').forEach((item, index) => item.id = index);
+}
+
+window.addEventListener('DOMContentLoaded', loadTask);
